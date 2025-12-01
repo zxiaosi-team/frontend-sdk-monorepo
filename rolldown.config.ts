00c1ba36e9d78f12f8656e56ab6b9cf0e542b0a3
zxiaosi <2692159310@qq.com>
2025-12-01 23:42:56 +0800

    feat(sdk): rolldown 中配置 rollup-plugin-node-externals 排除依赖

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'rolldown';
 import { dts } from 'rolldown-plugin-dts';
-import { dependencies, peerDependencies } from './package.json';
+import nodeExternals from 'rollup-plugin-node-externals';
 
 const config = defineConfig({
   platform: 'browser', // 作用于浏览器环境
   tsconfig: './tsconfig.json', // 指定 tsconfig 文件
   input: './src/index.ts', // 入口文件
-  external: [...Object.keys(dependencies), ...Object.keys(peerDependencies)], // 将 package.json 中的依赖设为外部依赖
-  plugins: [dts()],
+  plugins: [
+    nodeExternals(), // 排除 deps、peerDeps 中的依赖
+    dts(),
+  ],
   output: {
     dir: 'dist', // 输出目录
     format: 'es', // 输出格式为 ES 模块
