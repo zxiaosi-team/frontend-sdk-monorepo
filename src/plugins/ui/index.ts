ab8d2bf2be6c9befb4d5e6044d4d0954df50290f
zxiaosi <2692159310@qq.com>
2025-12-16 23:54:31 +0800

    fix(sdk): SdkUIPlugin 插件添加 NoPermission 组件

--- src/plugins/ui/index.ts
+++ src/plugins/ui/index.ts
@@ -2,6 +2,7 @@ import type { Plugin } from '@/types';
 import { merge } from 'es-toolkit';
 import { type ComponentType, createElement, type ReactElement } from 'react';
 
+import NoPermission from './noPermission';
 import NotFound from './notFound';
 
 interface UIOptions {
@@ -39,6 +40,7 @@ const SdkUIPlugin: Plugin<'ui'> = {
     // 默认插件配置
     const defaultOptions = {
       NotFound,
+      NoPermission,
 
       getComponent: (name) => {
         if (!name) throw new Error('Sdk: Component name cannot be empty');
---
+++ src/plugins/ui/noPermission/index.tsx
@@ -0,0 +1,6 @@
+/** 无权限组件 */
+const NoPermission: React.FC = () => {
+  return <div>无权限</div>;
+};
+
+export default NoPermission;
