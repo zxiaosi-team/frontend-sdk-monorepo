658a515c9e1f9cbdb9f9fb54e3121c432b6a936e
zxiaosi <2692159310@qq.com>
2025-12-20 19:52:11 +0800

    featsdk): SdkUIPlugin 中添加 Microapp 组件

---
+++ src/utils/constant.ts
@@ -0,0 +1,68 @@
+import type { FrameworkLifeCycles, ObjectType } from 'qiankun';
+
+import type { LocaleProps, SdkResult, ThemeProps } from '@/types';
+
+/** qiankun 生命周期 钩子函数 */
+export const lifeCyclesUtil: FrameworkLifeCycles<ObjectType> = {
+  beforeLoad: [
+    async (app) => {
+      console.log('[LifeCycle] before load %c%s', 'color: green;', app.name);
+    },
+  ],
+  beforeMount: [
+    async (app) => {
+      console.log('[LifeCycle] before mount %c%s', 'color: green;', app.name);
+    },
+  ],
+  afterUnmount: [
+    async (app) => {
+      console.log('[LifeCycle] after unmount %c%s', 'color: green;', app.name);
+    },
+  ],
+};
+
+/**
+ * 获取主题默认值
+ * @param sdk sdk
+ */
+export const getDefaultThemeUtil = (sdk: SdkResult): ThemeProps => {
+  // localStorage > sdk中主题 > 系统主题 > 默认
+
+  // 1. localStorage
+  const localTheme = sdk.storage.getTheme() as ThemeProps;
+  if (localTheme) return localTheme;
+
+  // 2. sdk中主题
+  const sdkTheme = sdk.config?.theme;
+  if (sdkTheme) return sdkTheme;
+
+  // 3. 系统主题
+  const media = window.matchMedia('(prefers-color-scheme: dark)');
+  if (media.matches) return media.matches ? 'dark' : 'light';
+
+  // 4. 默认
+  return 'light';
+};
+
+/**
+ * 获取国际化默认值
+ * @param sdk sdk
+ */
+export const getDefaultLocaleUtil = (sdk: SdkResult): LocaleProps => {
+  // localStorage > sdk中国际化 > 浏览器语言 > 默认
+
+  // 1. localStorage
+  const localLocale = sdk.storage.getLocale() as LocaleProps;
+  if (localLocale) return localLocale;
+
+  // 2. sdk中国际化
+  const sdkLocale = sdk.config?.locale;
+  if (sdkLocale) return sdkLocale;
+
+  // 3. 浏览器语言
+  const browserLocale = navigator.language as LocaleProps;
+  if (browserLocale) return browserLocale;
+
+  // 4. 默认
+  return 'zh-CN';
+};
