17eddea1d8b426b7bde1d2a3f643287e05265886
zxiaosi <2692159310@qq.com>
2025-12-25 02:01:11 +0800

    feat(sdk): SdkUIPlugin 中添加 Mainapp 组件

--- src/plugins/store/createTheme.ts
+++ src/plugins/store/createTheme.ts
@@ -1,9 +1,13 @@
 import type { StateCreator } from 'zustand';
 
+import { theme as antdTheme } from 'antd';
+import { merge } from 'es-toolkit';
+
 import type { ThemeProps } from '@/types';
 
 import { sdk } from '@/core';
 
+const { defaultAlgorithm, darkAlgorithm } = antdTheme;
 interface ThemeStoreProps {
   /** 主题 */
   theme: ThemeProps;
@@ -24,6 +28,10 @@ const createThemeSlice: StateCreator<ThemeStoreProps> = (set, get) => ({
 
     // 设置作用域
     document.documentElement.setAttribute('theme', theme);
+
+    // 设置Antd主题算法
+    const algorithm = theme === 'light' ? defaultAlgorithm : darkAlgorithm;
+    merge(sdk.config.antdConfig, { theme: { algorithm } });
   },
 });
 
