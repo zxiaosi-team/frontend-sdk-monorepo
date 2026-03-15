aad0d956d8b19b4121d0e9316913a0255cd118ab
zxiaosi <2692159310@qq.com>
2026-03-15 20:05:08 +0800

    fix: 使用 tsdown 作为打包工具

---
+++ tsdown.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsdown';
+
+const config = defineConfig({
+  entry: './src/index.ts', // 入口文件
+  platform: 'browser', // 作用于浏览器环境
+  tsconfig: './tsconfig.json', // 指定 tsconfig 文件
+  format: 'esm', // 输出格式
+  minify: true, // 压缩代码
+});
+
+export default config;
