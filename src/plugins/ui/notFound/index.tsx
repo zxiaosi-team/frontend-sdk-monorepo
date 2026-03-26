84f79ad6a32eeda7cb2d38dd2e9cbfa7988deed9
zxiaosi <2692159310@qq.com>
2026-03-26 22:30:57 +0800

    breaking(sdk): SdkUIPlugin 插件中 NotFound 组件去除 Antd 依赖引入

--- src/plugins/ui/notFound/index.tsx
+++ src/plugins/ui/notFound/index.tsx
@@ -1,25 +1,8 @@
-import { theme } from 'antd';
-
-const { useToken } = theme;
+import './index.css';
 
 /** 404组件 */
 const NotFound: React.FC = () => {
-  const { token } = useToken();
-
-  return (
-    <div
-      style={{
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        background: token.colorBgContainer,
-      }}
-    >
-      找不到页面
-    </div>
-  );
+  return <div className='sdk-notfound'>找不到页面</div>;
 };
 
 export default NotFound;
