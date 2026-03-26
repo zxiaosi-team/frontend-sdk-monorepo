cfb9ae0f6d59fc086948c43dbb2ae3cdf309d450
zxiaosi <2692159310@qq.com>
2026-03-26 22:34:56 +0800

    feat(sdk): 添加 useInitData hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,4 @@ export { useUserInfo } from './useUserInfo';
 export { useIntl } from './useIntl';
 export { useCrumb } from './useCrumb';
 export { usePermission } from './usePermission';
+export { useInitData } from './useInitData';
