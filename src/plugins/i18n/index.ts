a0a588e4b73f588ab80819cf901e90f2f981dbfa
zxiaosi <2692159310@qq.com>
2026-03-28 21:43:51 +0800

    deps(sdk): 移除 react-intl-universal 依赖

--- src/plugins/i18n/index.ts
+++ src/plugins/i18n/index.ts
@@ -1,16 +1,13 @@
 import { merge } from 'es-toolkit/object';
-import intl from 'react-intl-universal';
 
 import type { Plugin } from '@/types';
 
 interface I18nOptions {
   /**
-   * React Intl Universal 实例
-   * - 不要解构使用, const { get } = useIntl() 会报错
-   * - 如果项目不使用 React Compiler, 可以直接使用 sdk.i18n.intl
-   * @example const intl = useIntl(); intl.get(key).d(defaultValue)
+   * Intl 实例
+   * @example sdk.i18n.intl?.xxx
    */
-  intl?: typeof intl;
+  intl?: any;
   /**
    * 自定义语言包
    * @example
@@ -34,15 +31,13 @@ const pluginName = 'i18n';
 /**
  * 多语言
  * - 详情参考 {@link I18nOptions} {@link I18nResults}
- * - 集成 React Intl Universal
- * - 需要从外部引入语言包, 详见 intlConfig 和 loadLocale 配置项
  */
 const SdkI18nPlugin: Plugin<'i18n'> = {
   name: pluginName,
   install(sdk, options = {}) {
     // 默认插件配置
     const defaultOptions = {
-      intl: intl,
+      intl: {},
       intlConfig: {},
     } satisfies I18nResults;
 
