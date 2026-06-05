875127e7cbbc1a9e8f40648dddfea057c6492a24
zxiaosi <2692159310@qq.com>
2026-06-05 23:38:11 +0800

    fix(sdk): 移除 ApiOptions 中的 fetch 方法

--- src/plugins/api/index.ts
+++ src/plugins/api/index.ts
@@ -1,53 +1,17 @@
-import { merge } from 'es-toolkit/object';
-
 import type { SDKPlugin } from '@/types';
 
-interface ApiOptions {
-  /** 默认请求方法 */
-  fetch(url: string, options: RequestInit): Promise<any>;
-}
+interface ApiOptions {}
 
 /** 插件名称 */
 const pluginName = 'api';
 
 /**
  * 请求插件
- *
- * @example
- * sdk.use(SDKApiPlugin).mount('xxx');
- * await sdk.api.fetch('/api/data', { method: 'GET' });
- *
- * @example
- * sdk.use(SDKApiPlugin, {
- *  get: async (url, options) => {
- *     return sdk.api.fetch(url, { method: 'GET', ...options });
- *   }
- * });
- * await sdk.api.get('/api/data');
  */
 const SDKApiPlugin: SDKPlugin = {
   name: pluginName,
   install(sdk, options = {}) {
-    const defaultOptions = {
-      fetch: async (url: string, options: RequestInit = {}) => {
-        const { method, headers, ...rest } = options;
-
-        try {
-          const resp = await fetch(url, {
-            ...rest,
-            method,
-            headers: { 'Content-Type': 'application/json', ...headers },
-          });
-
-          return await resp.json();
-        } catch (error) {
-          console.error('SDK - Request failed:', url, error);
-          return Promise.reject(error);
-        }
-      },
-    } satisfies ApiOptions;
-
-    sdk[pluginName] = merge(defaultOptions, options);
+    sdk[pluginName] = options;
   },
 };
 
